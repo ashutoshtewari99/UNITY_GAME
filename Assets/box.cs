cb46e4c82f9f2f3633ce7a38a4d88ad8aa70aed2
ASHUTOSH TEWARI <54748312+ashutoshtewari99@users.noreply.github.com>
2020-05-31 01:18:18 +0530

    FIRST COMMIT
    ADDING UNITY PROJEXT TO GITHUB

---
+++ Assets/box.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box : MonoBehaviour
+{ 
+
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,20,2000 * Time.deltaTime);
+    }
+}
